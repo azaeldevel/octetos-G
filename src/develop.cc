be600a73cdf8b05de59be0742c62f239a0dec35a
Azael Reyes <azael.devel@gmail.com>
2021-09-18 19:16:38 -0700

    Adding color

--- src/develop.cc
+++ src/develop.cc
@@ -1,27 +1,82 @@
-/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
-/*
- * main.cc
- * Copyright (C) 2021 Azael Reyes <azael.devel@gmail.com>
- * 
- * octetos-render is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * octetos-render is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 
-#include <iostream>
 
-int main()
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+// Include GLM
+#include <glm/glm.hpp>
+using namespace glm;
+
+#include "space.hh"
+
+
+int main( void )
 {
-	std::cout << "Hello world!" << std::endl;
+	// Initialise GLFW
+	glewExperimental = true;
+	if( !glfwInit() )
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL);
+	if( window == NULL ){
+		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	
+	// Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW.\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Dark blue background
+	oct::space::Color<GLfloat> cleanColor(0.0f, 0.0f, 0.4f, 0.0f);
+	oct::space::clear(cleanColor);
+
+	do{
+		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+		glClear( GL_COLOR_BUFFER_BIT );
+
+		// Draw nothing, see you in tutorial 2 !
+
+		
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
 	return 0;
 }
-
---
+++ src/space.hh
@@ -0,0 +1,35 @@
+
+#ifndef OCTETOS_3D_HH
+#define OCTETOS_3D_HH
+
+namespace oct::space
+{
+
+template<typename T> struct Color
+{
+	T red;
+	T green;
+	T blue;
+	T alpha;
+
+	Color()
+	{
+	}
+	Color(T r,T g,T b,T a)
+	{
+		red = r;
+		green = g;
+		blue = b;
+		alpha = a;
+	}
+};
+
+template<typename T> void clear(const Color<T>& color)
+{
+	glClearColor(color.red,color.green,color.blue,color.alpha);
+}
+
+
+}
+
+#endif
