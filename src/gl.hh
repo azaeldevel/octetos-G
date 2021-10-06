cd515e9240e1e712d55caa8774f5d8b9c330e857
Azael Reyes <azael.devel@gmail.com>
2021-10-06 11:01:07 -0700

    working on cube
    Signed-off-by: Azael Reyes <azael.devel@gmail.com>

--- src/gl.hh
+++ src/gl.hh
@@ -9,27 +9,56 @@ namespace oct::gl
 
 template<typename T> struct Color
 {
-	T red;
-	T green;
-	T blue;
-	T alpha;
+	T r;
+	T g;
+	T b;
+	T a;
 
 	Color()
 	{
 	}
 	Color(T r,T g,T b,T a)
 	{
-		red = r;
-		green = g;
-		blue = b;
-		alpha = a;
+		this->r = r;
+		this->g = g;
+		this->b = b;
+		this->a = a;
+	}
+	Color(T r,T g,T b)
+	{
+		this->r = r;
+		this->g = g;
+		this->b = b;
+		this->a = 1;
 	}
 
 	void clear()
 	{
-		glClearColor(red,green,blue,alpha);
+		glClearColor(r,g,b,a);
 	}
+
+	Color inverse() const
+	{
+		Color color;
+		color.r = T(1) - r;
+		color.g = T(1) - g;
+		color.b = T(1) - b;
+		color.a = a;
+		return  color;
+	}
+
+	static const Color<T> white;
+	static const Color<T> black;
+	static const Color<T> red;
+	static const Color<T> green;
+	static const Color<T> blue;
 };
+template<typename T> const Color<T> Color<T>::white = Color(1,1,1);
+template<typename T> const Color<T> Color<T>::black = Color(0,0,0);
+template<typename T> const Color<T> Color<T>::red = Color(1,0,0);
+template<typename T> const Color<T> Color<T>::green = Color(0,1,0);
+template<typename T> const Color<T> Color<T>::blue = Color(0,0,1);
+
 
 template<typename T> struct Vertex2D
 {
@@ -45,6 +74,19 @@ template<typename T> struct Vertex3D : public Vertex2D<T>
 template<typename T> struct Triangle
 {
 	Vertex3D<T> vertex[3];//P1 es esquina inferior izquierda, luego la suoerior y al final la derecha
+
+	void set(const Color<T>& color)
+	{
+		vertex[0].x = color.r;
+		vertex[0].y = color.g;
+		vertex[0].z = color.b;
+		vertex[1].x = color.r;
+		vertex[1].y = color.g;
+		vertex[1].z = color.b;
+		vertex[2].x = color.r;
+		vertex[2].y = color.g;
+		vertex[2].z = color.b;
+	}
 };
 
 template<typename T> struct Square
@@ -54,6 +96,9 @@ template<typename T> struct Square
 	Square()
 	{
 	}
+	/**
+	*\brief P1 es vertice inferior izquierdo, P2 es vertice superio derecho
+	**/
 	Square(T x1,T y1,T z1,T x2,T y2,T z2)
 	{
 		set(x1,y1,z1,x2,y2,z2);
@@ -69,7 +114,7 @@ template<typename T> struct Square
 		triangles[0].vertex[1].z = z1;
 		triangles[0].vertex[2].x = x2;
 		triangles[0].vertex[2].y = y1;
-		triangles[0].vertex[2].z = z1;
+		triangles[0].vertex[2].z = z2;
 
 		//T2
 		triangles[1].vertex[0].x = x1;
@@ -81,6 +126,11 @@ template<typename T> struct Square
 		triangles[1].vertex[2].x = x2;
 		triangles[1].vertex[2].y = y1;
 		triangles[1].vertex[2].z = z2;
+	}
+	void set(const Color<T>& color)
+	{
+		triangles[0].set(color);
+		triangles[1].set(color);
 	}
 };
 
@@ -100,11 +150,30 @@ template<typename T> struct Cube
 		//Lado 3 : frente
 		square[2].set(x2,y1,z2,x1,y2,z2);
 		//Lado 4 : izquierdo
-		square[3].set(x1,y1,z1,x1,y2,z2);
+		square[3].set(x1,y1,z2,x1,y2,z1);
 		//Lado 6 : abajo
 		square[4].set(x1,y1,z1,x2,y1,z2);
 		//Lado 5 : arriba
 		square[5].set(x1,y2,z1,x2,y2,z2);
+	}
+	Cube(const Color<T> colors[6])
+	{
+		set(colors);
+	}
+	void set(const Color<T> colors[6])
+	{
+		//Lado 1 : atras
+		square[0].set(colors[0]);
+		//Lado 2 : derecho
+		square[1].set(colors[1]);		
+		//Lado 3 : frente
+		square[2].set(colors[2]);
+		//Lado 4 : izquierdo
+		square[3].set(colors[3]);
+		//Lado 6 : abajo
+		square[4].set(colors[4]);
+		//Lado 5 : arriba
+		square[5].set(colors[5]);
 	}
 };
 
